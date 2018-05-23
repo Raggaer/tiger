205a7cd55ba3a18b9e4c3cedac6f916196e990a7
Raggaer <nakotoffana@gmail.com>
2018-05-23 13:34:49 +0200

    Add usage render command

--- app/controllers/controller.go
+++ app/controllers/controller.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"database/sql"
+	"strings"
 	"time"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/raggaer/tiger/app/config"
 	"github.com/raggaer/tiger/app/xml"
 )
@@ -16,4 +18,45 @@ type Context struct {
 	Vocations map[string]*xml.Vocation
 	Items     map[int]xml.Item
 	DB        *sql.DB
+}
+
+// Command defines a discord command
+type Command struct {
+	Usage       string
+	Description string
+	Options     []CommandOption
+}
+
+// CommandOption defines a command option value
+type CommandOption struct {
+	Name        string
+	Usage       string
+	Description string
+}
+
+// RenderUsage sends a message with the command usage
+func (c *Command) RenderUsage(title string, ctx *Context, s *discordgo.Session, m *discordgo.MessageCreate) error {
+	msg := strings.Builder{}
+	msg.WriteString(c.Description)
+	msg.WriteString("\r\nCommand usage: `" + ctx.Config.Discord.Prefix + c.Usage + "`\r\n")
+	fields := []*discordgo.MessageEmbedField{}
+
+	// Add usage options if needed
+	if len(c.Options) > 0 {
+		for _, o := range c.Options {
+			fields = append(fields, &discordgo.MessageEmbedField{
+				Name:  o.Name,
+				Value: o.Description,
+			})
+		}
+	}
+
+	// Send usage message
+	_, err := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+		Color:       3447003,
+		Title:       title,
+		Description: msg.String(),
+		Fields:      fields,
+	})
+	return err
 }
--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 	}
 
 	// Wait here until CTRL-C or other term signal is received
-	log.Println("Tiger is now running. Press CTRL-C to exit.")
+	log.Println("Tiger is now running. Press CTRL-C to exit")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
