f68342f2344ceba6cd217bd5b2e11f0b438c1779
Raggaer <nakotoffana@gmail.com>
2018-05-23 13:35:11 +0200

    Update monders to use usage render Add monster view killed

--- app/controllers/monster.go
+++ app/controllers/monster.go
@@ -1,34 +1,43 @@
 package controllers
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/raggaer/tiger/app/models"
 	"github.com/raggaer/tiger/app/xml"
 )
+
+var monsterCommand = Command{
+	Usage: "monster Name, option",
+	Options: []CommandOption{
+		{
+			Name:        "info",
+			Description: "Returns basic information about the monster",
+		},
+		{
+			Name:        "loot",
+			Description: "Returns the loot table of the monster",
+		},
+		{
+			Name:        "killed",
+			Description: "Returns the last 10 players killed by the monster",
+		},
+	},
+}
 
 // ViewMonster returns information about the given monster
 func ViewMonster(context *Context, s *discordgo.Session, m *discordgo.MessageCreate) error {
 	data := strings.Split(m.Content, ",")
 	if len(data) <= 1 {
-		_, err := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-			Color:       3447003,
-			Title:       "View monster",
-			Description: "Not enough parameters. Command usage `" + context.Config.Discord.Prefix + "monster Name, option`",
-		})
-		return err
+		return monsterCommand.RenderUsage("Unknown option", context, s, m)
 	}
 
 	// Get monster
-	monster, ok := context.Monsters[strings.TrimSpace(data[0])]
+	monster, ok := context.Monsters[strings.ToLower(strings.TrimSpace(data[0]))]
 	if !ok {
-		_, err := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-			Color:       3447003,
-			Title:       "Monster not found",
-			Description: "Monster not found. Command usage `" + context.Config.Discord.Prefix + "monster Name, option`",
-		})
-		return err
+		return monsterCommand.RenderUsage("Monster not found", context, s, m)
 	}
 
 	// Switch monster view method
@@ -37,52 +46,84 @@ func ViewMonster(context *Context, s *discordgo.Session, m *discordgo.MessageCre
 		return viewMonsterLoot(context, s, m, monster)
 	case "info":
 		return viewMonsterInformation(context, s, m, monster)
+	case "killed":
+		return viewMonsterKilledPlayers(context, s, m, monster)
+	default:
+		return viewMonsterInformation(context, s, m, monster)
 	}
-	return nil
+}
+
+func viewMonsterKilledPlayers(context *Context, s *discordgo.Session, m *discordgo.MessageCreate, monster *xml.Monster) error {
+	deaths, err := models.GetPlayerDeathsByMonster(context.DB, monster)
+	if err != nil {
+		return err
+	}
+
+	// Render deaths message
+	msg := strings.Builder{}
+	if len(deaths) <= 0 {
+		msg.WriteString("\r\nNoone")
+	} else {
+		for _, d := range deaths {
+			msg.WriteString("\r\n- Killed **" + d.Player.Name + "** at level **" + strconv.Itoa(d.Level) + "**")
+		}
+	}
+
+	_, err = s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+		Color:       3447003,
+		Title:       "Players killed by " + monster.Description,
+		Description: msg.String(),
+	})
+	return err
 }
 
 func viewMonsterInformation(context *Context, s *discordgo.Session, m *discordgo.MessageCreate, monster *xml.Monster) error {
-	msg := "**You view " + monster.Description + "** \r\n \r\n"
-	msg += fmt.Sprintf("- **Experience**: %d \r\n", monster.Experience)
-	msg += fmt.Sprintf("- **Speed**: %d \r\n", monster.Speed)
-	msg += fmt.Sprintf("- **Health**: %d \r\n \r\n", monster.Health.Now)
+	msg := strings.Builder{}
+	msg.WriteString("**You view" + monster.Description + "** \r\n \r\n")
+	msg.WriteString("- **Experience**: " + strconv.Itoa(monster.Experience) + "\r\n")
+	msg.WriteString("- **Speed**: " + strconv.Itoa(monster.Speed) + " \r\n")
+	msg.WriteString("- **Health**: " + strconv.Itoa(monster.Health.Now) + " \r\n \r\n")
 	for _, attack := range monster.Attacks.Attacks {
-		msg += fmt.Sprintf("- **Attack**: %s (%d, %d)\r\n", attack.Name, attack.Min, attack.Max)
+		msg.WriteString("- **Attack**: " + attack.Name + " (" + strconv.Itoa(attack.Min) + ", " + strconv.Itoa(attack.Max) + ")\r\n")
 	}
+
 	// Send information message
 	_, err := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 		Color:       3447003,
 		Title:       monster.Name + " information",
-		Description: msg,
+		Description: msg.String(),
 	})
 	return err
 }
 
 func viewMonsterLoot(context *Context, s *discordgo.Session, m *discordgo.MessageCreate, monster *xml.Monster) error {
-	msg := ""
+	msg := strings.Builder{}
 	for _, item := range monster.Loot.Loot {
+
 		// Calculate item chance percentage
 		chance := 100.0
 		if item.Chance > 0 {
 			chance = float64(item.Chance) / 1000.0
 		}
+
 		// Check if we need to retrieve item from map
 		if item.Name == "" {
 			i, ok := context.Items[item.ID]
 			if !ok {
 				continue
 			}
-			msg += fmt.Sprintf("\r\n- **%s** - %.2f", i.Name, chance) + "%"
+			msg.WriteString("\r\n- **" + i.Name + "** - " + strconv.FormatFloat(chance, 'f', 2, 64) + "%")
 			continue
 		}
-		msg += fmt.Sprintf("\r\n- **%s** - %.2f", item.Name, chance) + "%"
+
+		msg.WriteString("\r\n- **" + item.Name + "** - " + strconv.FormatFloat(chance, 'f', 2, 64) + "%")
 	}
 
 	// Send loot message
 	_, err := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 		Color:       3447003,
 		Title:       monster.Name + " loot",
-		Description: msg,
+		Description: msg.String(),
 	})
 	return err
 }
